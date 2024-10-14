89bff33100483736f49382cef3ffd3963b0804bb
Salma_KR <48034001+SalmaKazemiRashed@users.noreply.github.com>
2024-10-14 21:24:23 +0200

    put all augmentation and other preprcoessing script in preprocessing

--- Augmentation/Random_augmentation.py
+++
@@ -1,9 +0,0 @@
- # For random augmentation: rotation (-20 to 20)/flipping/shifting/brightness change (0.8 to 1.3)
- datagen = ImageDataGenerator(
-        rotation_range=20,
-        width_shift_range=0.2,
-        height_shift_range=0.2,
-        horizontal_flip=True, 
-        fill_mode='nearest',
-        brightness_range=(0.8, 1.3),validation_split= False)
-       
