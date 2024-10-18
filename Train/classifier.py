291605fbf3e2a885e9d20f55afbda6184d8aaf70
Salma_KR <48034001+SalmaKazemiRashed@users.noreply.github.com>
2024-10-18 17:53:55 +0200

    update results

--- Train/classifier.py
+++ Train/classifier.py
@@ -16,6 +16,64 @@ from Preprocessing import Manual_augmentation.ImageAugmentor  ##Different augmen
 from Preprocessing import Random_augmentation.ImageAugmentor
 from Preprocessing import brightness_blurred_augmentation.ImageAugmentor
 
+
+## functions and objects required for trackcing results in each epoch (callbacks)
+##########################augmentation############################
+##################################################################
+class MulticlassTruePositives(tf.keras.metrics.Metric):
+    def __init__(self, name='multiclass_true_positives', **kwargs):
+        super(MulticlassTruePositives, self).__init__(name=name, **kwargs)
+        self.true_positives = self.add_weight(name='tp', initializer='zeros')
+
+    def update_state(self, y_true, y_pred, sample_weight=None):
+        y_pred = tf.reshape(tf.argmax(y_pred, axis=1), shape=(-1, 1))
+        values = tf.cast(y_true, 'int32') == tf.cast(y_pred, 'int32')
+        values = tf.cast(values, 'float32')
+        if sample_weight is not None:
+            sample_weight = tf.cast(sample_weight, 'float32')
+            values = tf.multiply(values, sample_weight)
+        self.true_positives.assign_add(tf.reduce_sum(values))
+
+    def result(self):
+        return self.true_positives
+
+    def reset_state(self):
+        # The state of the metric will be reset at the start of each epoch.
+        self.true_positives.assign(0.)
+
+##################################################################
+class PerformanceVisualizationCallback(Callback):
+    def __init__(self, model, validation_data, image_dir):
+        super().__init__()
+        self.model = model
+        self.validation_data = validation_data
+        
+        os.makedirs(image_dir, exist_ok=True)
+        self.image_dir = image_dir
+
+    def on_epoch_end(self, epoch, logs={}):
+        y_pred = np.asarray(self.model.predict(self.validation_data[0]))
+        y_true = self.validation_data[1]             
+        y_pred_class = np.argmax(y_pred, axis=1)
+
+        # plot and save confusion matrix
+        fig, ax = plt.subplots(figsize=(16,12))
+        plot_confusion_matrix(y_true, y_pred_class,labels=[0,1],true_labels=[0,1], ax=ax)
+        fig.savefig(os.path.join(self.image_dir, f'confusion_matrix_epoch_{epoch}'))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # Load the parameters from the JSON file
 with open('config.json', 'r') as f:
     config = json.load(f)
